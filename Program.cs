0ad9b6d6a928a0c4de58ed30dba80aecdcbc117f
Equith <62998421+Equith@users.noreply.github.com>
2020-06-13 17:44:25 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleAppExperiments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human person;
+            person = new Human();
+            person.Name = "Jarik";
+            person.Surname = "The Pro";
+            person.Age = 25;
+            person.EyeColor = "Pink";
+            person.Introduce();
+
+            person.Name = "Kolya";
+            person.Introduce();
+        }
+    }
+
+
+}
